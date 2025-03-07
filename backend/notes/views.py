538a5af8f4cdb4142082c7d8348329b6d4f002df
herman katende <hermankats16@gmail.com>
2025-03-07 04:30:02 +0300

    modified views, urls

--- backend/notes/views.py
+++ backend/notes/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework import generics
+from .models import Note
+from .serializers import NoteSerializer
+
+class NoteListCreateView(generics.ListCreateAPIView):
+    queryset = Note.objects.all()
+    serializer_class = NoteSerializer
+
+class NoteDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Note.objects.all()
+    serializer_class = NoteSerializer
