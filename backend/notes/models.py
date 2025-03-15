f9e37233d6823db203a48e6296e1f834d585ac23
herman katende <hermankats16@gmail.com>
2025-03-16 00:37:24 +0300

    creating the user and notes models

--- backend/notes/models.py
+++ backend/notes/models.py
@@ -8,11 +8,14 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
 class User(AbstractUser):  # Extending the default Django User model
     email = models.EmailField(unique=True)
 
 class Note(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)  # Link each note to a user
     title = models.CharField(max_length=200)
-    content = models.TextField()
+    content = models.TextField(null=True)  # Set an empty string as the default
     created_at = models.DateTimeField(auto_now_add=True)
