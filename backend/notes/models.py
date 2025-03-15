bd235b273013b099ff4e216970cc61ec1ff026aa
herman katende <hermankats16@gmail.com>
2025-03-16 00:41:05 +0300

    creating the user and notes models

--- backend/notes/models.py
+++ backend/notes/models.py
@@ -19,3 +19,6 @@ class Note(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField(default="")
     created_at = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.title
