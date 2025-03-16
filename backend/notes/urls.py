4cb68bdaf938738878d09b4001ec65023e85b3fb
Otana <emmaodongo10@gmail.com>
2025-03-16 09:32:10 +0300

    working on ux

--- backend/notes/urls.py
+++ backend/notes/urls.py
@@ -4,6 +4,17 @@ from .views import *
 from django.urls import path
 from .views import get_notes
 
+from django.urls import path
+from .views import (
+    RegisterView,
+    LoginView,
+    NoteListCreateView,
+    NoteRetrieveUpdateDestroyView
+)
+
 urlpatterns = [
-    path('notes/', get_notes, name='get_notes'),  # This is the endpoint to get all notes
+    path('api/users/register/', RegisterView.as_view(), name='register'),
+    path('api/users/login/', LoginView.as_view(), name='login'),
+    path('api/notes/', NoteListCreateView.as_view(), name='note-list'),
+    path('api/notes/<int:pk>/', NoteRetrieveUpdateDestroyView.as_view(), name='note-detail'),
 ]
