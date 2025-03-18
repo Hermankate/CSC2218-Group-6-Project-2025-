4b08a968e4ef911e665e072a479832015ac1aed8
Otana <emmaodongo10@gmail.com>
2025-03-19 00:31:13 +0300

    modified urls

--- backend/notes/urls.py
+++ backend/notes/urls.py
@@ -1,14 +1,15 @@
 from django.urls import path
+from django.urls import path
 from .views import (
     RegisterView,
-    LoginView,
+    sync_notes,
     NoteListCreateView,
     NoteRetrieveUpdateDestroyView
 )
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
-    path('login/', LoginView.as_view(), name='login'),
+    path('sync/', sync_notes, name='sync-notes'),
     path('notes/', NoteListCreateView.as_view(), name='note-list'),
     path('notes/<int:pk>/', NoteRetrieveUpdateDestroyView.as_view(), name='note-detail'),
 ]
